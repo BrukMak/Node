de739986b846e254072414a573011ba4510af0f2
BrukMak <60716114+BrukMak@users.noreply.github.com>
2024-07-04 00:52:04 +0300

    set up react frontend

--- Final-Project/frontend/src/App.js
+++ Final-Project/frontend/src/App.js
@@ -1,44 +1,23 @@
-import { Container, Grow, AppBar, Typography, Grid } from "@material-ui/core";
-import Student from "./components/showStudent/showStudent";
-import useStyles from "./styles"
-import CreateStudent from "./components/createStudent/createStudent";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const classes = useStyles();
   return (
     <div className="App">
-      <Container max-width="lg">
-        <AppBar className={classes.appBar} position="static" color="inherit">
-          <Typography className={classes.heading} variant="h2" align="center">
-            Student Management System
-          </Typography>
-        </AppBar>
-        <Grow in>
-          <Container>
-            <Grid container justify="space-between" alignItems="stretch">
-              <Grid item xs={12} sm={7}>
-                <AppBar
-                  className={classes.appBar}
-                  position="static"
-                  color="inherit"
-                >
-                  <Student />
-                </AppBar>
-              </Grid>
-              <Grid item xs={12} sm={4}>
-              <AppBar
-                  className={classes.appBar}
-                  position="static"
-                  color="inherit"
-                >
-                  <CreateStudent />
-                </AppBar>
-              </Grid>
-            </Grid>
-          </Container>
-        </Grow>
-      </Container>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
